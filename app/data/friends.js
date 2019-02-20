6583002237e367a3b0e3a4cac1103b0acb6be4e0
Tracy Mah <tracy.y.mah@gmail.com>
2019-02-20 17:57:17 -0500

    homwork update

---
+++ app/data/friends.js
@@ -0,0 +1,100 @@
+var friends = [
+    {
+      name: "Doraemon",
+      photo: "https://c7.uihere.com/files/501/361/242/doraemon-desktop-wallpaper-anime-instant-film-doraemon.jpg",
+      scores: [
+        1,
+        4,
+        2,
+        5,
+        2,
+        3,
+        3,
+        1,
+        1,
+        5
+    },
+    {
+      name: "Nobita",
+      photo: "http://www.goodmorningimagesforlover.com/wp-content/uploads/2018/09/nobita-picture.jpg",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Takeshi",
+      photo: "https://vignette.wikia.nocookie.net/scratchpad/images/9/9d/Takeshi_Gouda.png/revision/latest?cb=20171102024300",
+      scores: [
+        "5",
+        "1",
+        "5",
+        "1",
+        "5",
+        "1",
+        "5",
+        "1",
+        "5",
+        "1"
+      ]
+    },
+    {
+      name: "Dorami",
+      photo: "https://2.bp.blogspot.com/-pnF4Qt8b-1I/VzD8Mwk-ldI/AAAAAAAA_34/xEu2aXliqVoKvewFntojmzfEpu9Q2v2wwCKgB/s1600/05_15_PERSONAJES_WEB_DORAEMON_BRUSH_BOING_DORAMI.png",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Shizuki",
+      photo: "https://vignette.wikia.nocookie.net/doraemon/images/7/71/C0E297DC-08AE-43E6-841D-41E95B2FC8D0.jpeg/revision/latest?cb=20181218213552&path-prefix=en",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "3",
+        "2",
+        "3",
+        "3",
+        "4",
+        "3",
+        "3"
+      ]
+    },
+    {
+      name: "Suneo",
+      photo: "https://static.giantbomb.com/uploads/scale_small/0/7135/2648451-suneohonekawa.jpg",
+      scores: [
+        "5",
+        "1",
+        "5",
+        "3",
+        "4",
+        "2",
+        "4",
+        "3",
+        "5",
+        "3"
+      ]
+    }
+  
+  ];
+  module.exports = friends;
+  
